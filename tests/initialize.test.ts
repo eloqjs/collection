80855defe805e0a8d5b325f1881e2f76c1e866cf
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2020-11-10 18:15:56 -0300

    test(initialize): test `items` property of Collection

--- tests/initialize.test.ts
+++ tests/initialize.test.ts
@@ -5,7 +5,6 @@ describe('Initialize Collection', () => {
     const collection = new Collection()
 
     expect(collection).toBeInstanceOf(Collection)
-
     expect(collect()).toBeInstanceOf(Collection)
   })
 
@@ -14,7 +13,6 @@ describe('Initialize Collection', () => {
     const collection = new Collection(...array)
 
     expect(collection).toEqual(array)
-
     expect(collect(...array)).toEqual(array)
   })
 
@@ -23,7 +21,30 @@ describe('Initialize Collection', () => {
     const collection = new Collection(array)
 
     expect(collection).toEqual(array)
-
     expect(collect(array)).toEqual(array)
+  })
+
+  it('Should return itself using items property', () => {
+    const array = [{}, {}, {}]
+    const collection = new Collection(array)
+
+    expect(collection.items).toEqual(array)
+    expect(collect(array).items).toEqual(array)
+  })
+
+  it('Should mutate itself using items property', () => {
+    const array1 = [{}, {}, {}]
+    const array2 = [{}, {}, {}, {}, {}, {}]
+    const collection1 = new Collection(array1)
+    const collection2 = collect(array1)
+
+    expect(collection1.items).toEqual(array1)
+    expect(collection2.items).toEqual(array1)
+
+    collection1.items = collect(array2)
+    collection2.items = collect(array2)
+
+    expect(collection1.items).toEqual(array2)
+    expect(collection2.items).toEqual(array2)
   })
 })
