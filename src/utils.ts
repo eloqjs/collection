1a8dcc8c3f395f28954352d12ede7fb99b1dd0f1
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2020-11-10 17:36:23 -0300

    feat(utils): add utils `nestedValue`, `variadic`, `isArray`, `isObject` and `isFunction`

---
+++ src/utils.ts
@@ -0,0 +1,61 @@
+/**
+ * Get value of a nested property.
+ *
+ * @param mainObject
+ * @param {string} key
+ * @returns {*}
+ */
+export function nestedValue(mainObject: never, key: string): any {
+  try {
+    return key.split('.').reduce((obj, property) => obj[property], mainObject)
+  } catch (err) {
+    // If we end up here, we're not working with an object, and @var mainObject is the value itself
+    return mainObject
+  }
+}
+
+/**
+ * Variadic helper function.
+ *
+ * @param args
+ * @returns {*}
+ */
+export function variadic<T>(args: T[] | [T[]]): T[] {
+  if (Array.isArray(args[0])) {
+    return args[0]
+  }
+
+  return args as T[]
+}
+
+/**
+ * The isArray() method determines whether the passed value is an Array.
+ *
+ * @param {*} item
+ * @returns {boolean}
+ */
+export function isArray(item: unknown): item is Array<any> {
+  return Array.isArray(item)
+}
+
+/**
+ * The isObject() method determines whether the passed value is an Object.
+ *
+ * @param {*} item
+ * @returns {boolean}
+ */
+export function isObject(item: unknown): item is Record<string, any> {
+  return (
+    typeof item === 'object' && Array.isArray(item) === false && item !== null
+  )
+}
+
+/**
+ * The isFunction() method determines whether the passed value is a Function.
+ *
+ * @param {*} item
+ * @returns {boolean}
+ */
+export function isFunction(item: unknown): item is (...args: any[]) => any {
+  return typeof item === 'function'
+}
