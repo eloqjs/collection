6fc4fe4a56a0107255a47c392a3802e36b80adf1
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2020-11-16 23:24:44 -0300

    feat: add `min`

--- src/Collection.ts
+++ src/Collection.ts
@@ -541,6 +541,22 @@ export default class Collection<Item extends ItemData = ItemData> extends Array<
     ) as number
   }
 
+  /**
+   * The min method returns the minimum value of a given key.
+   *
+   * @param {string|string[]} key
+   * @return {number}
+   */
+  min<K extends KeyVariadic>(key: keyof Item | K): number {
+    const filtered = this.items.filter(
+      (item) => getProp(item, key as KeyVariadic) !== undefined
+    )
+
+    return Math.min(
+      ...filtered.map((item) => getProp(item, key as KeyVariadic) as number)
+    )
+  }
+
   pluck<V extends Key>(value: keyof Item | V): unknown[]
   pluck<V extends Key, K extends Key>(
     value: keyof Item | V,
--- tests/methods.test.ts
+++ tests/methods.test.ts
@@ -1041,6 +1041,86 @@ describe('Public Methods', () => {
     })
   })
 
+  describe('min()', () => {
+    it('should return the minimum value of a given key', () => {
+      const data = [
+        {
+          worth: 100
+        },
+        {
+          worth: 900
+        },
+        {
+          worth: 79
+        },
+        {
+          worth: undefined
+        },
+        {
+          unicorn: false
+        }
+      ]
+
+      const collection = collect(data)
+      const minKey = collection.min('worth')
+      expect(minKey).toEqual(79)
+      expect(collection).toEqual(data)
+    })
+
+    it('should return the minimum value of a given nested key', () => {
+      const data = [
+        {
+          foo: {
+            worth: 100
+          }
+        },
+        {
+          foo: {
+            worth: 900
+          }
+        },
+        {
+          foo: {
+            worth: 79
+          }
+        },
+        {
+          foo: {
+            worth: undefined
+          }
+        },
+        {
+          unicorn: false
+        }
+      ]
+
+      const collection = collect(data)
+      const minKey = collection.min('foo.worth')
+      expect(minKey).toEqual(79)
+      expect(collection).toEqual(data)
+    })
+
+    it('should work with negative values', () => {
+      const data = [
+        { value: 1 },
+        { value: 2 },
+        { value: 3 },
+        { value: 4 },
+        { value: 5 },
+        { value: -5 },
+        { value: -4 },
+        { value: -3 },
+        { value: -2 },
+        { value: -1 }
+      ]
+      const collection = collect(data)
+      const min = collection.min('value')
+
+      expect(collection).toEqual(data)
+      expect(min).toEqual(-5)
+    })
+  })
+
   describe('pluck()', () => {
     const products = [
       { product: 'Desk', price: 200, manufacturer: 'IKEA' },
